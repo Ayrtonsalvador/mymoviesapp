ec7f138df98d033ffd3954171ba3a87b4b0264dc
Ayrton Salvador <salvador.ayrton@gmail.com>
2021-12-10 17:37:38 +0100

    Update connection.js
    Added secret API

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
 
+var USERNAME = USER_NAME;
+var PASSWORD = PASS_WORD;
+
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,      
     useUnifiedTopology : true}
 
-mongoose.connect(`mongodb+srv://admin:coucou@cluster0.isz5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
+mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.isz5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
     options, 
     function(err) {  
         console.log(err);} );
